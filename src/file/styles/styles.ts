78825a22b0dbdbf804b657343ca5877e30f446f7
Bruce Duncan <bduncan@rassilon.co>
2020-01-29 19:18:04 -0500

    Update to merge of 5.0.0-rc7

--- src/file/styles/styles.ts
+++ src/file/styles/styles.ts
@@ -1,5 +1,4 @@
 import { BaseXmlComponent, ImportedXmlComponent, XmlComponent } from "file/xml-components";
-import { DocumentAttributes } from "../document/document-attributes";
 import { CharacterStyle, ParagraphStyle } from "./style";
 import { ICharacterStyleOptions } from "./style/character-style";
 import { IParagraphStyleOptions } from "./style/paragraph-style";
@@ -18,17 +17,6 @@ export class Styles extends XmlComponent {
 
         if (options.initialStyles) {
             this.root.push(options.initialStyles);
-        } else {
-            this.root.push(
-                new DocumentAttributes({
-                    mc: "http://schemas.openxmlformats.org/markup-compatibility/2006",
-                    r: "http://schemas.openxmlformats.org/officeDocument/2006/relationships",
-                    w: "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
-                    w14: "http://schemas.microsoft.com/office/word/2010/wordml",
-                    w15: "http://schemas.microsoft.com/office/word/2012/wordml",
-                    Ignorable: "w14 w15",
-                }),
-            );
         }
 
         if (options.importedStyles) {
