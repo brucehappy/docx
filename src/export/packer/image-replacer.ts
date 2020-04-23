d3981fec9b965d41d9d92b9f7e3f5a44945f80cb
Bruce Duncan <bduncan@rassilon.co>
2020-04-23 19:28:56 -0400

    Since the image and numbering replacers are trying to replace attribute values in XML strings, we need to ensure we include the surrounding double quotes (that assures us of matching an entire attribute value as double quotes are escaped in XML)  We also need to xml escape the string we are trying to find and replace, as well as escape the xml escaped string for use as regular expression (we use a RegExp since String.replace will only do the first instance if given a string).

--- src/export/packer/image-replacer.ts
+++ src/export/packer/image-replacer.ts
@@ -1,17 +1,23 @@
 import { IMediaData, Media } from "file/media";
+import * as xml from "xml";
 
 export class ImageReplacer {
     public replace(xmlData: string, mediaData: IMediaData[], offset: number): string {
         let currentXmlData = xmlData;
+        const escapeRegExp = (s) => s.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
 
         mediaData.forEach((image, i) => {
-            currentXmlData = currentXmlData.replace(new RegExp(`{${image.fileName}}`, "g"), (offset + i).toString());
+            // Must use RegExp to replace all instances of the string.
+            currentXmlData = currentXmlData.replace(
+                new RegExp(escapeRegExp(`"rId{${xml(image.fileName)}}"`), "g"),
+                `"rId${(offset + i).toString()}"`,
+            );
         });
 
         return currentXmlData;
     }
 
     public getMediaData(xmlData: string, media: Media): IMediaData[] {
-        return media.Array.filter((image) => xmlData.search(`{${image.fileName}}`) > 0);
+        return media.Array.filter((image) => xmlData.indexOf(`"rId{${xml(image.fileName)}}"`) !== -1);
     }
 }
--- src/export/packer/numbering-replacer.ts
+++ src/export/packer/numbering-replacer.ts
@@ -1,15 +1,21 @@
 import { ConcreteNumbering } from "file";
+import * as xml from "xml";
 
 export class NumberingReplacer {
     public replace(xmlData: string, concreteNumberings: ConcreteNumbering[]): string {
         let currentXmlData = xmlData;
+        const escapeRegExp = (s) => s.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
 
         for (const concreteNumbering of concreteNumberings) {
             if (!concreteNumbering.reference) {
                 continue;
             }
 
-            currentXmlData = currentXmlData.replace(new RegExp(`{${concreteNumbering.reference}}`, "g"), concreteNumbering.id.toString());
+            // Must use RegExp to replace all instances of the string.
+            currentXmlData = currentXmlData.replace(
+                new RegExp(escapeRegExp(`"{${xml(concreteNumbering.reference)}}"`), "g"),
+                `"${concreteNumbering.id.toString()}"`,
+            );
         }
 
         return currentXmlData;
